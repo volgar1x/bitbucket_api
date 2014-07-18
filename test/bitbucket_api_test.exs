5ba657f2bf60dcb3f01423dfd16ff3ecfe91e833
Blackrush <blackrushx@gmail.com>
2014-07-18 23:19:56 +0200

    add a small test

--- test/bitbucket_api_test.exs
+++ test/bitbucket_api_test.exs
@@ -6,4 +6,9 @@ defmodule BitbucketApiTest do
     assert params_to_string([]) == ""
     assert params_to_string([milestone: "alpha"]) == "?milestone=alpha"
   end
+
+  test "hackney options" do
+    assert to_hackney_options([]) == []
+    assert to_hackney_options([user: "foo", pass: "bar"]) == [basic_auth: {"foo", "bar"}]
+  end
 end
